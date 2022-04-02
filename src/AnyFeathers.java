48587a99ff9584c680aadc07d1e9eac3f35af8e3
jdaubinet <52372978+jdaubinet@users.noreply.github.com>
2022-04-02 16:26:13 +0200

    First commit

---
+++ src/AnyFeathers.java
@@ -0,0 +1,184 @@
+import com.sun.xml.internal.bind.v2.model.annotation.Quick;
+import nox.scripts.noxscape.core.api.QuickExchange;
+import nox.scripts.noxscape.util.Sleep;
+import nox.scripts.noxscape.util.Trader;
+
+import org.osbot.rs07.api.GrandExchange;
+import org.osbot.rs07.api.Inventory;
+import org.osbot.rs07.api.map.Position;
+import org.osbot.rs07.api.model.Item;
+import org.osbot.rs07.api.model.NPC;
+import org.osbot.rs07.api.ui.RS2Widget;
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+
+
+@ScriptManifest(author = "anykeys", name="Feathers", version = 0.0, info = "", logo = "")
+public class AnyFeathers  extends Script {
+
+
+    private QuickExchange ge;
+    private Trader trader;
+    private int state = 0;
+
+    private final Position[] locations = { new Position(3013,3223, 0), new Position(3164, 3485, 0)};
+    @Override
+    public void onStart() throws InterruptedException {
+        ge = new QuickExchange(this);
+        trader = new Trader(this);
+    }
+
+    @Override
+    public int onLoop() throws InterruptedException {
+
+        Item coins = getInventory().getItem("Coins");
+        Item packs = getInventory().getItem("Feather pack");
+        NPC gerrant = getNpcs().closest("Gerrant");
+        RS2Widget shop = getWidgets().get(300, 16, 9);
+
+        if(myPosition().getArea(10).contains(gerrant.getPosition()))
+            state = 1;
+
+
+        if(coins == null)
+            stop(true);
+
+        if(state == 0 ) // at GE
+        {
+            if( !myPlayer().getArea(5).contains( new Position(3164, 3485, 0)) && coins.getAmount() <= 200)
+            {
+                Item featherpack = getInventory().getItem("Feather pack");
+
+                if(featherpack != null)
+                    featherpack.interact("Open");
+
+                Sleep.until(() -> featherpack == null || featherpack.getAmount() < 1, 3000);
+
+                if(featherpack == null)
+                    walking.webWalk(new Position(3164, 3485, 0));
+            }
+            else
+                state = 2;
+
+        }else if( state == 1) // at Shop
+        {
+
+
+
+
+            if(shop == null) {
+                if(gerrant != null) {
+                    gerrant.interact("Trade");
+                    Sleep.until(() -> !myPlayer().isMoving(), 3000);
+                }
+                if(!myPosition().getArea(10).contains(new Position(3013, 3223, 0)))
+                    walking.webWalk(new Position(3013, 3223, 0));
+
+            }else
+            {
+
+
+
+                if(!inventory.isFull()  )
+                {
+                    if( coins.getAmount() > 200) {
+                        if (shop.getItemAmount() == 100)
+                            shop.interact("Buy 10");
+                    }else
+                    if(shop.isVisible())
+                        getWidgets().get(300, 1, 11).interact("Close");
+                }else
+                {
+                    if(shop.isVisible())
+                        getWidgets().get(300, 1, 11).interact("Close");
+
+                    packs.interact("Open");
+
+                    Sleep.until(()-> packs == null, 20000);
+                }
+                if(!getInventory().isFull() && coins.getAmount() < 200)
+                    state = 0;
+
+            }
+        }
+        else if ( state == 2) // at ge get bank
+        {
+            if(bank.closest() == null )
+                return 500;
+            if(!bank.isOpen())
+                bank.open();
+
+            if(bank.open()) {
+                Sleep.until(() -> bank.withdrawAll("Feather"), 1000, 1000);
+
+
+                bank.close();
+
+                state = 3;
+            }
+        }
+        else if ( state == 3 ){ // sell on ge
+
+            if (!ge.isOpen())
+                ge.open();
+
+            if (ge.isOpen()) {
+                Item feather = getInventory().getItem("Feather");
+                if (feather != null)
+                    Sleep.until(() -> {
+                        return grandExchange.sellItem(feather.getId(), 3, feather.getAmount());
+                    }, 5000);
+
+
+
+                Sleep.until(()->grandExchange.collect(), 1000, 1000);
+                state = 1;
+                if(ge.isOpen())
+                    grandExchange.close();
+            }
+
+
+
+
+        }
+
+
+
+
+
+//        if(worlds.getCurrentWorld() != 382) {
+//            getWorlds().hop(382);
+//
+//            Sleep.until(() -> worlds.getCurrentWorld() == 382, 5000);
+//        }
+
+
+
+
+
+//    if( coins != null && coins.getAmount() - 500000 > 0) {
+//        if(trader.trade("lilibaggins"))
+//        {
+//            if(trader.offerItem(coins, coins.getAmount() - 500000))
+//            {
+//                wait(5000);
+//                if(trader.acceptTrade(true)) {
+//                    log("trade complete");
+//                    this.stop(true);
+//                }
+//            }
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+        return 1000;
+    }
+}
---
+++ src/nox/scripts/noxscape/util/Trader.java
@@ -0,0 +1,188 @@
+package nox.scripts.noxscape.util;
+
+import org.osbot.rs07.api.filter.Filter;
+import org.osbot.rs07.api.model.Item;
+import org.osbot.rs07.api.model.Player;
+import org.osbot.rs07.api.ui.RS2Widget;
+import org.osbot.rs07.script.MethodProvider;
+import org.osbot.rs07.utility.ConditionalSleep;
+
+public class Trader {
+
+    private MethodProvider mp;
+
+    private RS2Widget inTradeWidget;
+    private RS2Widget amountWidget;
+    private RS2Widget declineButton;
+    private RS2Widget acceptButton;
+    private RS2Widget acceptButtonSelected;
+
+    public Trader(MethodProvider mp) {
+        this.mp = mp;
+    }
+
+    private boolean inputXAmount(int amount) {
+        new ConditionalSleep(10000, 100) {
+            @Override
+            public boolean condition() throws InterruptedException {
+                return ((amountWidget = mp.getWidgets().get(162, 44)) != null) && amountWidget.isVisible();
+            }
+        }.sleep();
+        if (amountWidget != null) {
+            return mp.keyboard.typeString("" + amount);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean acceptTrade(boolean acceptAllScreens) {
+        new ConditionalSleep(5000, 100) {
+            @Override
+            public boolean condition() throws InterruptedException {
+                return ((acceptButton = mp.getWidgets().getWidgetContainingText("Accept")) != null) && acceptButton.isVisible();
+            }
+        }.sleep();
+        if (acceptButton != null) {
+            if (acceptButton.interact("Accept")) {
+                new ConditionalSleep(10000, 100) {
+                    @Override
+                    public boolean condition() throws InterruptedException {
+                        return ((acceptButtonSelected = mp.getWidgets().getWidgetContainingText("Waiting for other player...")) != null) && acceptButtonSelected.isVisible();
+                    }
+                }.sleep();
+                new ConditionalSleep(100000, 1500) {
+                    @Override
+                    public boolean condition() throws InterruptedException {
+                        return !acceptButton.isVisible() || acceptButtonSelected.getMessage().equals("");
+                    }
+                }.sleep();
+                if (acceptButtonSelected.getMessage().equals("")) {
+                    acceptTrade(acceptAllScreens);
+                }
+                if (acceptAllScreens) {
+                    acceptTrade(true);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean declineTrade() {
+        if ((declineButton = mp.getWidgets().getWidgetContainingText("Decline")) != null) {
+            if (declineButton.interact("Decline")) {
+                new ConditionalSleep(100000, 100) {
+                    @Override
+                    public boolean condition() throws InterruptedException {
+                        return !declineButton.isVisible();
+                    }
+                }.sleep();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean offerAll(String itemName) {
+        if (mp.getWidgets().getWidgetContainingText("Trading with: ") != null) {
+            return mp.getInventory().interact("Offer-All", itemName);
+        }
+        return false;
+    }
+
+    public boolean offerAll(Item item) {
+        if (mp.getWidgets().getWidgetContainingText("Trading with: ") != null) {
+            return item.interact("Offer-All");
+        }
+        return false;
+    }
+
+    public boolean offerItem(String itemName, int amount) {
+        String amountSelection;
+        if (amount == 1) {
+            amountSelection = "Offer";
+        } else if (amount == 5) {
+            amountSelection = "Offer-5";
+        } else if (amount == 10) {
+            amountSelection = "Offer-10";
+        } else {
+            amountSelection = "Offer-X";
+        }
+        if (mp.getWidgets().getWidgetContainingText("Trading with: ") != null) {
+            if (mp.getInventory().interact(amountSelection, itemName)) {
+                if (amountSelection.equals("Offer-X")) {
+                    return inputXAmount(amount);
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean offerItem(Item item, int amount) {
+        if (item == null) {
+            return false;
+        }
+        String amountSelection;
+        if (amount == 1) {
+            amountSelection = "Offer";
+        } else if (amount == 5) {
+            amountSelection = "Offer-5";
+        } else if (amount == 10) {
+            amountSelection = "Offer-10";
+        } else {
+            amountSelection = "Offer-X";
+        }
+        if (mp.getWidgets().getWidgetContainingText("Trading with: ") != null) {
+            if (item.interact(amountSelection)) {
+                if (amountSelection.equals("Offer-X")) {
+                    return inputXAmount(amount);
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean trade(Player player) {
+        if (player == null) {
+            return false;
+        }
+        if (((inTradeWidget = mp.getWidgets().getWidgetContainingText("Accept")) != null) && inTradeWidget.isVisible()) {
+            return true;
+        }
+        if (player.interact("Trade with")) {
+            new ConditionalSleep(10000, 100) {
+                @Override
+                public boolean condition() throws InterruptedException {
+                    return ((inTradeWidget = mp.getWidgets().getWidgetContainingText("Accept")) != null);
+                }
+            }.sleep();
+            return inTradeWidget != null;
+        }
+        return false;
+    }
+
+    public boolean trade(String playerName) {
+        Player player = mp.players.closest((Filter<Player>) player1 -> player1.getName().equals(playerName));
+        if (((inTradeWidget = mp.getWidgets().getWidgetContainingText("Accept")) != null) && inTradeWidget.isVisible()) {
+            return true;
+        }
+        if (player != null) {
+            if (player.interact("Trade with")) {
+                new ConditionalSleep(10000, 100) {
+                    @Override
+                    public boolean condition() throws InterruptedException {
+                        return ((inTradeWidget = mp.getWidgets().getWidgetContainingText("Accept")) != null);
+                    }
+                }.sleep();
+                return inTradeWidget != null;
+            }
+        }
+        return false;
+    }
+
+
+}
